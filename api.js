b84d3e1658a75e9984fe12138f265185e02f44a6
Paul Bernhardt <paul@mappedin.ca>
2017-01-04 14:52:29 -0500

    Updated docs to 1.8.2

---
+++ api.js
@@ -0,0 +1,26 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Analytics",
+        "CameraControls",
+        "Easing",
+        "MapView",
+        "MapView2D",
+        "MapView3D",
+        "Mappedin",
+        "Search",
+        "Tween"
+    ],
+    "modules": [
+        "Mappedin"
+    ],
+    "allModules": [
+        {
+            "displayName": "Mappedin",
+            "name": "Mappedin",
+            "description": "The base Mappedin module, that has references to all the other Mappedin objects you need.\n\nStart with Mappedin.initalize, which will get the data for a Venue from the Mappedin API, and set up a MapView, Search, and Analytics object for you."
+        }
+    ],
+    "elements": []
+} };
+});
