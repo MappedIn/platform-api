4192e53722b809b4061f31272ea5400126ccddaf
Christine Maiolo <maiolo.c313@gmail.com>
2018-10-15 14:59:15 -0400

    Release of v1.46.1 docs

---
+++ api.js
@@ -0,0 +1,40 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Analytics",
+        "CameraControls",
+        "Easing",
+        "MapView",
+        "MapView2D",
+        "MapView3D",
+        "Mappedin",
+        "MappedinCategory",
+        "MappedinDirections",
+        "MappedinDirective",
+        "MappedinEvent",
+        "MappedinLocation",
+        "MappedinLocationState",
+        "MappedinMap",
+        "MappedinMapGroup",
+        "MappedinNode",
+        "MappedinPolygon",
+        "MappedinVenue",
+        "MappedinVortex",
+        "Marker",
+        "Search",
+        "TextLabel",
+        "Tween"
+    ],
+    "modules": [
+        "Mappedin"
+    ],
+    "allModules": [
+        {
+            "displayName": "Mappedin",
+            "name": "Mappedin",
+            "description": "The base Mappedin module, that has references to all the other Mappedin objects you need.\n\nStart with Mappedin.initialize, which will get the data for a Venue from the Mappedin API, and set up a MapView, Search, and Analytics object for you."
+        }
+    ],
+    "elements": []
+} };
+});
